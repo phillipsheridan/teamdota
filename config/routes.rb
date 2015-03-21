9804e3b958109a7a4e6129e502d63f5b0edc0787
wisard1862 <danieswain@gmail.com>
2015-03-21 06:01:03 -0400

    Added home view, Fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-
+  get 'static_pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -10,8 +8,8 @@ Rails.application.routes.draw do
  
 
   Rails.application.routes.draw do
-  
-  root 'welcome#index'
+
+  root 'static_pages#home'
 end
 
 
