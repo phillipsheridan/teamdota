825d89e2a6d70b65ebf3bfea148c931c88e8826c
wisard1862 <danieswain@gmail.com>
2015-03-26 10:05:11 -0400

    Added validators to topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,14 @@
 class Topic < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, length: {minimum: 2}
+  validates :title, length: {maximum: 140}
+  
+  validates :handle, presence: true
+  validates :handle, length: {in: 2..20}
+  validates :handle, uniqueness: true
+
+  validates :game_name, presence: true
+  validates :game_name, length: {minimum: 2}
+  validates :game_name, length: {maximum: 20}
+
 end
