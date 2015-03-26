0b59d4cab014be8797d9b28b693175c4b7c0fc16
wisard1862 <danieswain@gmail.com>
2015-03-26 09:25:49 -0400

    Added Description to Topic

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326131431) do
+ActiveRecord::Schema.define(version: 20150326132316) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
     t.string   "game_name"
     t.string   "handle"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
