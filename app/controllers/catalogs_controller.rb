2f4a7ce0785866a4b8422a06f4f4e3f2729c1184
wisard1862 <danieswain@gmail.com>
2015-03-30 03:23:22 -0400

    Edited show function

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,6 +1,7 @@
 class CatalogsController < ApplicationController
   def show
     @catalog = Catalog.find(params[:id])
+    @topics = Topic.where(game_name: @catalog.game_name)
   end
 
   def create
