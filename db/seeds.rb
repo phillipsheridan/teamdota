75a3c4ad7e9d86aa99eceeb52e9fde81cb1d533b
wisard1862 <danieswain@gmail.com>
2015-04-15 22:18:24 -0400

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+catalog_list = [
+	["dota2", "Defense of the Ancients 2"],
+	["lol", "League of Legends"],
+	["hon", "Heroes of Newerth"],
+	["smite", "SMITE"],
+	["hos", "Heroes of the Storm"]
+]
+
+catalog_list.each do |game_name, catalog_name|
+	Catalog.create(game_name: game_name, catalog_name: catalog_name)
+end
