bd0a5c3a2f3d41c9f390fbab423fefaf39e39631
wisard1862 <danieswain@gmail.com>
2015-04-16 09:38:09 -0400

    Make better

--- db/migrate/20150331214654_add_index_to_catalogs_game_name.rb
+++ db/migrate/20150331214654_add_index_to_catalogs_game_name.rb
@@ -1,5 +1,5 @@
 class AddIndexToCatalogsGameName < ActiveRecord::Migration
   def change
-    add_index :catalogs, :game_name, unique: true
+    #add_index :catalogs, :game_name, unique: true
   end
 end
