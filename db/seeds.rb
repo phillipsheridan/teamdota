2d08845386c3885e27ed6a0ea39b1a88d65849e3
wisard1862 <danieswain@gmail.com>
2015-04-16 10:54:17 -0400

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@ catalog_list = [
 ]
 
 lol_topics = [
-	["Looking for ADCarry", "lol", "Chancey"],
-	["Running 3v3s", "lol", "shutsUup"],
-	["Fun group, need supp", "lol", "RunsWithEmu"]
+	["Looking for ADCarry", "lol", "Chancey", "Need a good carry for our team. It's gonna be fun! Promise."],
+	["Running 3v3s", "lol", "shutsUup", "Needs 2 others for a real good time."],
+	["Fun group, need supp", "lol", "RunsWithEmu", "Looking for super good support. Pref have good heroes."]
 ]
 
 catalog_list.each do |game_name, catalog_name|
 	Catalog.create(game_name: game_name, catalog_name: catalog_name)
 end
 
-lol_topics.each do |title, game_name, handle|
-	Topic.create(title: title, game_name: game_name, handle: handle, description: )
+lol_topics.each do |title, game_name, handle, descrpt|
+	Topic.create(title: title, game_name: game_name, handle: handle, description: descrpt)
 end
